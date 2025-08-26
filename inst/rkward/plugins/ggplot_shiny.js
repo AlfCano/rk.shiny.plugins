a73d943a53533abb2a3ed345fb9237822c97d046
AlfCano <alfonsociologo@gmail.com>
2025-08-26 09:07:33 -0600

    Upload first draft of Shiny Plugins
    Update first draft.

---
+++ inst/rkward/plugins/ggplot_shiny.js
@@ -0,0 +1,30 @@
+// this code was generated using the rkwarddev package.
+// perhaps don't make changes here, but in the rkwarddev script instead!
+
+
+
+function preprocess(is_preview){
+	// add requirements etc. here
+	echo("require(ggplotgui)\n");
+}
+
+function calculate(is_preview){
+	// read in variables from dialog
+
+
+	// the R code to be evaluated
+
+    var data_frame = getValue("data_slot");
+    echo('result <- ggplot_shiny(dataset = ' + data_frame + ')\n');
+  
+}
+
+function printout(is_preview){
+	// printout the results
+
+    echo('rk.header(\"Lanzando Interfaz de ggplot\")\n');
+    echo('print(result)\n');
+  
+
+}
+
---
+++ inst/rkward/plugins/rpivotTable.js
@@ -0,0 +1,42 @@
+// this code was generated using the rkwarddev package.
+// perhaps don't make changes here, but in the rkwarddev script instead!
+
+
+
+function preprocess(is_preview){
+	// add requirements etc. here
+	echo("require(rpivotTable)\n");
+}
+
+function calculate(is_preview){
+	// read in variables from dialog
+
+
+	// the R code to be evaluated
+
+    var data_frame = getValue("data_slot");
+    echo('result <- rpivotTable(data = ' + data_frame + ')\n');
+  
+}
+
+function printout(is_preview){
+	// printout the results
+
+    var object_name = getValue("save_pivot.objectname");
+    var workplace = getValue("save_pivot.parent");
+    var full_object_name = workplace + '$' + object_name;
+    echo('rk.header(\"Tabla Dinámica Interactiva\")\n');
+    echo('print(result)\n');
+  
+	//// save result object
+	// read in saveobject variables
+	var savePivot = getValue("save_pivot");
+	var savePivotActive = getValue("save_pivot.active");
+	var savePivotParent = getValue("save_pivot.parent");
+	// assign object to chosen environment
+	if(savePivotActive) {
+		echo(".GlobalEnv$" + savePivot + " <- result\n");
+	}
+
+}
+
