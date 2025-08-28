96c04ef0db2aff0c2f318015a5db39499a960170
AlfCano <alfonsociologo@gmail.com>
2025-08-27 21:38:05 -0600

    update labels
    Update

--- inst/rkward/plugins/rpivotTable.js
+++ inst/rkward/plugins/rpivotTable.js
@@ -22,12 +22,8 @@ function calculate(is_preview){
 function printout(is_preview){
 	// printout the results
 
-    var object_name = getValue("save_pivot.objectname");
-    var workplace = getValue("save_pivot.parent");
-    var full_object_name = workplace + '$' + object_name;
     echo('rk.header(\"Interactive Pivot Table\")\n');
     echo('print(result)\n');
-    echo(full_object_name + ' <- result\n');
   
 	//// save result object
 	// read in saveobject variables
@@ -36,7 +32,7 @@ function printout(is_preview){
 	var savePivotParent = getValue("save_pivot.parent");
 	// assign object to chosen environment
 	if(savePivotActive) {
-		echo(".GlobalEnv$" + savePivot + " <- my.pivot\n");
+		echo(".GlobalEnv$" + savePivot + " <- result\n");
 	}
 
 }
